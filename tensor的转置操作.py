12a697bd9f2db68511be6eeb1972ef70bda94f39
gaoli1537 <774300381@qq.com>
2018-12-25 08:38:11 +0800

    test

---
+++ tensor的转置操作.py
@@ -0,0 +1,29 @@
+import tensorflow as tf 
+x=tf.constant(
+    [
+    [1,2,3],
+    [4,5,6]
+    ],tf.float16)
+
+r=tf.transpose(x,perm=[1,0])
+
+
+
+
+x_3_3_4=tf.constant(
+    [
+        [[1,2,3,4],[5,6,7,8],[9,10,11,12]],
+        [[13,14,15,16],[17,18,19,20],[21,22,23,24]],
+        [[25,26,27,28],[29,30,31,32],[33,34,35,36]]
+    ],tf.float16)
+
+x_3_3_4_shape=tf.shape(x_3_3_4)
+
+x_2_2_1=tf.reshape(x_3_3_4,[2,2,-1])
+x_9_9_1=tf.reshape(x_3_3_4,[4,3,3])
+session=tf.Session()
+print(session.run(x_3_3_4_shape))
+print(session.run(x_3_3_4))
+print(session.run(x_2_2_1))
+print(session.run(x_9_9_1))
+
