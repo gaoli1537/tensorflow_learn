e3ff9e77eba47e14ec911a2049058f476594b717
gaoli1537 <774300381@qq.com>
2018-12-25 08:45:15 +0800

    test1

---
+++ 卷积的作用.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+
+input1=tf.Variable(tf.constant(1.0,shape=[1,5,5,1]))
+input2=tf.Variable(tf.constant(1.0,shape=[1,5,5,2]))
+input3=tf.Variable(tf.constant(1.0,shape=[1,4,4,1]))
+
+
